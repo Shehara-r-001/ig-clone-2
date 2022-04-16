c256b315f5adab2db23bcd98a02d88b3705c1b5f
Shehara_r <ranathisarashehara01@gmail.com>
2022-04-16 10:24:13 +0530

    add post header

--- components/addPost/AddNewPost.js
+++ components/addPost/AddNewPost.js
@@ -1,13 +1,44 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { ChevronLeftIcon } from 'react-native-heroicons/outline';
+import Upload from './Upload';
 
 const AddNewPost = () => {
   return (
-    <View>
-      <Text style={{ color: 'white' }}>AddNewPost</Text>
-    </View>
+    <>
+      <Header />
+      <Upload />
+    </>
   );
 };
+
+const Header = () => (
+  <View style={styles.container}>
+    <TouchableOpacity>
+      <ChevronLeftIcon size={30} style={{ color: 'white', marginLeft: 10 }} />
+    </TouchableOpacity>
+    <Text style={styles.headerText}>Add new post</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 5,
+  },
+  headerText: {
+    color: 'white',
+    width: '80%',
+    alignContent: 'center',
+    fontSize: 16,
+    textTransform: 'uppercase',
+    // justifyContent: 'space-around',
+    // flex: 1,
+    // textAlign: 'center',
+    fontWeight: '600',
+  },
+});
 
 export default AddNewPost;
--- screens/AddPostScreen.js
+++ screens/AddPostScreen.js
@@ -1,10 +1,11 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StatusBar } from 'react-native';
 import React from 'react';
 import AddNewPost from '../components/addPost/AddNewPost';
 
 const AddPostScreen = () => {
   return (
     <SafeAreaView style={{ backgroundColor: 'black', flex: 1 }}>
+      <StatusBar backgroundColor='#000000' barStyle='light-content' />
       <AddNewPost />
     </SafeAreaView>
   );
