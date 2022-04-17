74e9382a65f14d5f5241b896c4c683fe717dff01
Shehara_r <ranathisarashehara01@gmail.com>
2022-04-17 10:53:48 +0530

    yup validation

--- components/addPost/Upload.js
+++ components/addPost/Upload.js
@@ -1,12 +1,19 @@
 import { View, Text, Image, TextInput, StyleSheet } from 'react-native';
 import React from 'react';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  imageUrl: Yup.string().url().required('Image URL is required'),
+  caption: Yup.string().max(1500, 'Maximum number of characters reached'),
+});
 
 const Upload = () => {
   return (
     <Formik
       initialValues={{ caption: '', imageUrl: '' }}
       onSubmit={(values) => console.log(values)}
+      validationSchema={schema}
     >
       {({
         handleBlur,
@@ -29,12 +36,24 @@ const Upload = () => {
             placeholder='Enter the caption..'
             placeholderTextColor='gray'
             style={[styles.textInput, { marginTop: 10 }]}
+            multiline={true}
+            onChangeText={handleChange('caption')}
+            onBlur={handleBlur('caption')}
+            value={values.caption}
           />
           <TextInput
             placeholder='Enter the image URL..'
             placeholderTextColor='gray'
             style={styles.textInput}
+            onChangeText={handleChange('imageUrl')}
+            onBlur={handleBlur('imageUrl')}
+            value={values.imageUrl}
           />
+          {errors.imageUrl && (
+            <Text style={{ color: 'red', fontSize: 12 }}>
+              {errors.imageUrl}
+            </Text>
+          )}
         </>
       )}
     </Formik>
